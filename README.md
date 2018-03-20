99850c899c0cd1de91c84657bc8376f6e0258060
junyiz <junyime@qq.com>
2018-03-20 16:55:37 +0800

    refactor the code

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # dature
-新浪博客抓取小程序
-
+抓取新浪博客的数据，包含标题、正文、时间、分类、图片，并生成HTML文件
 
 ## 安装
 
@@ -8,30 +7,12 @@
 npm install -g dature 
 ```
 
-
 ## 使用
 
-### 抓取新浪博客的数据（标题、正文、时间、分类、图片），保存为blog/data.json，并生成HTML文件
 ```bash
-dature -cu sina_blog_uid
-```
-或 
-```bash
-dature --create --uid sina_blog_uid
+dature sina_blog_uid
 ```
 
+## 查看新浪博客的UID，如下图
 
-### 只抓取新浪博客的数据（标题、正文、时间、分类、图片），保存为blog/data.json
-```bash
-dature -u sina_blog_uid
-```
-
-
-### 只生成HTML文件
-```bash
-dature -j blog/data.json
-```
-或 
-```bash
-dature --json blog/data.json
-```
+![](http://github.com/junyiz/dature/raw/master/example.jpg)
--- index.js
+++ index.js
@@ -1,130 +1,48 @@
 #!/usr/bin/env node
-let fs = require('fs');
-let ejs = require('ejs');
-let urlParse = require('url').parse;
-let path = require('path');
-let dfmt = require('dfmt');
-let zenio = require('zenio');
-let cheerio = require('cheerio');
-let mkdirp = require('mkdirp');
-let isUrl = require('is-url');
-let cwd = process.cwd();
-let join = path.join;
-let headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'};
-let blogDir = join(cwd, './blog');
-let argv = require('yargs')
-  .option('u', {
-    alias : 'uid',
-    demand: false,
-    requiresArg: true,
-    describe: '新浪博客作者的uid',
-    type: 'string'
-  })
-  .option('c', {
-    alias : 'create',
-    boolean: true,
-    describe: '生成HTML页面'
-  })
-  .option('j', {
-    alias : 'json',
-    demand: false,
-    requiresArg: true,
-    describe: '新浪博客数据文件',
-    type: 'string'
-  })
-  .usage('Usage: dature [options]')
-  .example('dature -c -u 1234567890')
-  .help('h')
-  .alias('h', 'help')
-  .epilog('@copyright 2017 junyiz')
-  .argv;
+
+const fs = require('fs')
+const ejs = require('ejs')
+const util = require('util')
+const path = require('path')
+const mkdirp = require('mkdirp')
+const join = path.join
+const readFile = util.promisify(fs.readFile)
+const writeFile = util.promisify(fs.writeFile)
+const extract = require('./lib/extract')
+const pictureDownloader = require('./lib/pictureDownloader')
+const blogDir = join(process.cwd(), './blog')
+const uid = process.argv[2]
 
 async function fetch(uid) {
-  let blogUrl = `http://blog.sina.com.cn/s/articlelist_${uid}_0_1.html`;
-  let data = {urls: [], blog: [], imgs: {}, date: dfmt('yyyy-MM-dd hh:mm:ss')};
-  let addUrl = $ => {
-    $('.articleList a[title]').each((i, el) => {
-      let href = $(el).attr('href');
-      if (isUrl(href)) data.urls.push(href);
-    });
-  };
+  // 生成目录
+  mkdirp.sync(blogDir)
+  mkdirp.sync(blogDir + '/imgs')
+  mkdirp.sync(blogDir + '/post')
 
-  //抓取博客的URL
-  let list = await zenio.get(blogUrl, null, headers).catch(console.log);
-  let $ = cheerio.load(list);
-  addUrl($);
-  let page = ($('span[style]').text().match(/\d+/) || [0])[0]; //总页数
-  data.title = $('title').text().split('_')[1];
-  data.link = $('.blogtitle a').attr('href');
-  for (let i = 2; i <= page && page > 1; i++) {
-    let list = await zenio.get(blogUrl.replace(/1.html/, i + '.html'), null, headers).catch(console.log);
-    let $ = cheerio.load(list);
-    addUrl($);
-  }
+  // 获取数据
+  let data = await extract(uid)
 
-  //抓取博客
-  for (let i = 0; i < data.urls.length; i++) {
-    let link = data.urls[i];
-    let post = await zenio.get(link, null, headers).catch(console.log);
-    let $ = cheerio.load(post, {decodeEntities: false});
-    let title = $('.artical .titName').text();
-    let date = $('.artical .time.SG_txtc').text().replace(/(\(|\))/g, '');
-    let cate = $('.blog_class a').text();
-    let content = $('div.articalContent').html()
-      .replace(/<(p)[^>]*/gi, '<$1')
-      .replace(/\n?<\/?(span|font)[^>]*>\n?/gi, '')
-      .replace(/<a[^>]*><\/a>/gi, '')
-      .replace(/<img[^>]*real_src="([^"]*)"([^>]*)>/gi, (m, a, b) => {
-        a = a.replace(/&amp;\d*/gi, '');
-        let base = path.parse(urlParse(a).pathname).base;
-        data.imgs[base] = a;
-        return `<img src="${base}"${b}>`;
-      })
-      .replace(/<a[^>]*>(<img[^>]*>)<\/a>/gi, '$1')
-      .trim();
-    data.blog.push({title, date, cate, link, content});
-  }
+  // 写入数据
+  writeFile(join(blogDir, 'data.json'), JSON.stringify(data), 'utf8')
 
-  //写入数据
-  fs.writeFileSync(join(blogDir, 'data.json'), JSON.stringify(data), 'utf8');
+  // 批量下载图片
+  data.imgs.map(({url, name}) => {
+    pictureDownloader(url, join(blogDir, 'imgs', name))
+  })
 
-  //抓取图片
-  zenio.setOptions({encoding: 'binary'});
-  for (let i in data.imgs) {
-    let img;
-    try {
-      img = await zenio.get(data.imgs[i], null, headers);
-    } catch(e) {
-      continue;
-    }
-    img && fs.writeFileSync(join(blogDir, i), img, 'binary');
-  }
-
-  return data;
-}
-
-//生成HTML
-function createHtml(data) {
-  let tplIndex = fs.readFileSync(__dirname + '/tpl/index.html', 'utf8');
-  let tplBlog = fs.readFileSync(__dirname + '/tpl/blog.html', 'utf8');
-  //index.html
-  fs.writeFileSync(join(blogDir, 'index.html'), ejs.render(tplIndex, data), 'utf8');
-  //blog[n].html
-  for (let i = 0; i < data.blog.length; i++) {
-    fs.writeFileSync(join(blogDir, `blog${i + 1}.html`), ejs.render(tplBlog, data.blog[i]), 'utf8');
+  // 生成HTML
+  let tplIndex = await readFile(__dirname + '/tpl/index.html', 'utf8')
+  let tplBlog = await readFile(__dirname + '/tpl/blog.html', 'utf8')
+  // index.html
+  writeFile(join(blogDir, 'index.html'), ejs.render(tplIndex, data), 'utf8')
+  // post/[n].html
+  for (let i = 0; i < data.post.length; i++) {
+    await writeFile(join(blogDir, `/post/${i + 1}.html`), ejs.render(tplBlog, data.post[i]), 'utf8')
   }
 }
 
-if (argv.u && argv.c) {
-  mkdirp.sync(blogDir);
-  let uid = argv.u || argv.c;
-  fetch(uid).then(createHtml, console.log);
-} else if (argv.u) {
-  mkdirp.sync(blogDir);
-  fetch(argv.u).catch(console.log);
-} else if (argv.j) {
-  let dataFile = join(cwd, argv.j);
-  blogDir = path.dirname(dataFile);
-  mkdirp.sync(blogDir);
-  createHtml(require(dataFile));
+if (/\d{10}/.test(uid)) {
+  fetch(uid)
+} else {
+  console.info(`\n请输入正确的新浪博客UID, 例如 dature 1263917762\n`)
 }
