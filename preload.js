bd24daa92cb611c11c0dbdd8e9566cab1a61a721
junyiz <junyime@qq.com>
2022-06-03 16:59:05 +0800

    build macOS version

--- preload.js
+++ preload.js
@@ -1,6 +1,7 @@
 const fetch = require('./lib/fetch')
 const join = require('path').join
 const { shell } = require('electron')
+const { help } = require('yargs')
 
 const $ = (selector) => document.getElementById(selector)
 
@@ -15,11 +16,18 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const dir = join(process.cwd(), `./blog-${uid}`)
 
-    console.log = (log) => out.innerHTML += log.replace('\n', '<br>')
+    console.log = (log) => out.innerHTML += log.replace('\n', '<br>') + '<br>'
 
     fetch(dir, uid, cookie).then(function() {
       console.log(`\n备份完毕, 博客存储目录：<a href="${dir}/index.html">${dir}</a>\n`)
     })
+  })
+
+  $('help').addEventListener('click', (event) => {
+    if (event.target.tagName === 'A') {
+      event.preventDefault()
+      shell.openExternal(event.target.href)
+    }
   })
 
   out.addEventListener('click', (event) => {
