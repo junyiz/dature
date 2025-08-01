87d1bb09a7bc81f49658bacb4a55a01f20d9b387
Junyi <junyime@qq.com>
2025-08-01 21:32:49 +0800

    update

--- srv.js
+++ srv.js
@@ -1,6 +1,6 @@
 import http from 'http'
 import { parse } from 'url'
-import { join } from 'path'
+// import { join } from 'path'
 import fetch from './lib/fetch.js'
 
 export const parseSearch = (search) => {
@@ -19,7 +19,7 @@ const server = http.createServer((req, res) => {
   if (!u && !c) return
 
   const uid = u
-  const dir = join(process.cwd(), `./blog/${uid}`)
+  const dir = `${process.env.ROOT}/blog/${uid}`
   const cookie = c.replace(/(NowDate|BLOG_TITLE|mblog_userinfo)[^;]*;/g, '')
 
   // 设置响应头以启用 SSE
