0b4012a54f05dfdd931ecdc6e5f1d5405afcb26e
junyiz <junyime@qq.com>
2025-08-01 17:04:36 +0800

    update yargs

--- cli.js
+++ cli.js
@@ -5,25 +5,22 @@ const yargs = require('yargs')
 const fetch = require('./lib/fetch')
 const package = require('./package.json')
 
-let argv = yargs
-  .option('u', {
-    alias : 'uid',
-    demand: false,
-    requiresArg: true,
-    describe: '博客uid',
-    type: 'string'
-  })
-  .option('c', {
-    alias : 'cookie',
-    demand: false,
-    requiresArg: false,
-    describe: '登录后的cookie',
-    type: 'string'
-  })
+let argv = yargs(process.argv.slice(2))
   .usage('Usage: dature [options]')
-  .help('h')
-  .alias('h', 'help')
-  .example('dature -u 1263917762')
+  .options({
+    'uid': {
+      alias : 'u',
+      demandOption: true,
+      describe: '博客uid',
+    },
+    'cookie': {
+      alias : 'c',
+      demandOption: true,
+      describe: '登录后的cookie',
+    }
+  })
+  .help()
+  .example('dature -uid 1263917762 -cookie "xxxxxxxxx"')
   .epilog('@junyiz')
   .argv;
 
@@ -39,8 +36,6 @@ if (argv.uid) {
   fetch(dir, argv.uid, cookie).then(() => {
     console.info(`\n备份还在继续，请等待程序结束\n`)
   })
-} else {
-  yargs.showHelp()
 }
 
 /*
