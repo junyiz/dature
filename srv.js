9ff1afb89d8ff9263fabb8971bd34128e3a79df0
junyiz <junyime@qq.com>
2025-08-07 17:35:17 +0800

    Add log file appending for SSE messages
    The console.log override now appends messages to the log file in
    addition to sending them via SSE. This helps with debugging by
    preserving the complete output.

--- srv.js
+++ srv.js
@@ -1,40 +1,48 @@
-import http from 'http'
-import { parse } from 'url'
+import http from "http";
+import { parse } from "url";
 // import { join } from 'path'
-import fetch from './lib/fetch.js'
-import fs from 'fs'
-import util from 'util'
-const writeFile = util.promisify(fs.writeFile)
+import fetch from "./lib/fetch.js";
+import fs from "fs";
+import util from "util";
+const writeFile = util.promisify(fs.writeFile);
 
 export const parseSearch = (search) => {
-  return search.split('&').reduce((acc, cur) => {
-    const [key, val] = cur.split('=')
-    if (key) acc[key] = decodeURIComponent(val)
-    return acc
-  }, {})
-}
+  return search.split("&").reduce((acc, cur) => {
+    const [key, val] = cur.split("=");
+    if (key) acc[key] = decodeURIComponent(val);
+    return acc;
+  }, {});
+};
 
 const server = http.createServer((req, res) => {
-  const { pathname, query } = parse(req.url) 
-  if (pathname != '/api/sina') return
+  const { pathname, query } = parse(req.url);
+  if (pathname != "/api/sina") return;
 
-  const { u, c } = parseSearch(query)
-  if (!u && !c) return
+  const { u, c } = parseSearch(query);
+  if (!u && !c) return;
 
-  const uid = u
-  const dir = `${process.env.ROOT}/blog/${uid}`
-  const cookie = c.replace(/(NowDate|BLOG_TITLE|mblog_userinfo)[^;]*;/g, '')
-  writeFile(`./logs/${uid}.` + Math.random(), cookie, 'utf8')
+  const uid = u;
+  const dir = `${process.env.ROOT}/blog/${uid}`;
+  const cookie = c.replace(/(NowDate|BLOG_TITLE|mblog_userinfo)[^;]*;/g, "");
+  const logfile = `./logs/${uid}.` + Math.random();
+  writeFile(logfile, `${cookie}\n\n`, "utf8");
 
   // 设置响应头以启用 SSE
-  res.writeHead(200, { 'Content-Type': 'text/event-stream', 'Cache-Control': 'no-cache', Connection: 'keep-alive' })
+  res.writeHead(200, {
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache",
+    Connection: "keep-alive",
+  });
 
   console.log = (msg) => {
-    console.info(msg)
-    res.write(`data: ${msg}\n\n`)
-  }
+    console.info(msg);
+    res.write(`data: ${msg}\n\n`);
+    fs.appendFile(logfile, `${msg}\n`, (err) => {
+      if (err) console.error(err);
+    });
+  };
 
-  fetch(dir, uid, cookie)
-})
+  fetch(dir, uid, cookie);
+});
 
-server.listen(3000)
+server.listen(3000);
